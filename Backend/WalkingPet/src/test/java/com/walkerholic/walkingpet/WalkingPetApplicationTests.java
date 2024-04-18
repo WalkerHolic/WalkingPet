e2df2457190052b981ae99dd7ddabbc7649c13da
신유진 <yujin1698@naver.com>
2024-04-18 19:52:00 +0900

    Chore: 스프링부트 백엔드 프로젝트 초기 세팅

---
+++ Backend/WalkingPet/src/test/java/com/walkerholic/walkingpet/WalkingPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkerholic.walkingpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
