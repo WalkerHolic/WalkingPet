df7fed5f7de434106262b2731f81c5d88402743e
신유진 <yujin1698@naver.com>
2024-04-22 00:22:08 +0900

    Feat: 사용자의 캐릭터 정보 가져오기 기본 틀 작성

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/controller/CharacterController.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/controller/CharacterController.java
@@ -1,9 +1,14 @@
 package com.walkerholic.walkingpet.domain.character.controller;
 
-import com.walkerholic.walkingpet.domain.character.service.CharacterService;
+import com.walkerholic.walkingpet.domain.character.dto.response.UserCharacterResponse;
+import com.walkerholic.walkingpet.domain.character.service.UserCharacterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/character")
 public class CharacterController {
 
-    private final CharacterService characterService;
+    private final UserCharacterService characterService;
 
+    @GetMapping
+    public ResponseEntity<UserCharacterResponse> getUserCharacterInfo(@RequestParam int userCharacterId) {
+        log.info("CharacterController getUserCharacterInfo - userCharacterId: {}", userCharacterId);
+
+        UserCharacterResponse userCharacterInfo = characterService.getUserCharacterInfo(userCharacterId);
+        return new ResponseEntity<>(userCharacterInfo, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        System.out.println("통신 테스트");
+        return new ResponseEntity<>("통신 테스트", HttpStatus.OK);
+    }
 
 }
