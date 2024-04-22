e8a582f528f1e43c6f38c336636ca363d7cafcfa
정연우 <yoanwoo@naver.com>
2024-04-22 17:18:48 +0900

    [BE]feat : GroupUser 엔티티 구현
    특정 그룹에 포함된 사용자들의 데이터를 보관하는  group_user 테이블과 연결되는 GroupUser 엔티티 구현 완료.

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/group/entity/GroupUser.java
@@ -0,0 +1,28 @@
+package com.walkerholic.walkingpet.domain.group.entity;
+
+import com.walkerholic.walkingpet.domain.users.entity.Users;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "group_user")
+public class GroupUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_user_id")
+    private Integer groupUserId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+}
