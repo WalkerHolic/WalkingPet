2a84ef09643216f9db87bd7472222ff2c38153b4
정연우 <yoanwoo@naver.com>
2024-04-23 23:05:38 +0900

    [BE]fix : UserItem 리스트 반환하는 메소드 수정
    JOIN FETCH를 통해 userId에 해당하는 UserItem 데이터를 가져오도록 수정하였다.

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/item/repository/UserItemRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/item/repository/UserItemRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserItemRepository extends JpaRepository<UserItem, Long> {
-    @Query("SELECT ui FROM UserItem ui WHERE ui.item.itemId IN :itemIds AND ui.user.userId = :userId")
-    Optional<List<UserItem>> findByItemItemIdAndUserUserIdIn(List<Integer> itemIds, Integer userId);
+
+    @Query("SELECT ui FROM UserItem ui JOIN FETCH ui.user WHERE ui.user.userId = :userId")
+    Optional<List<UserItem>> findByUserIdWithUserFetch(Integer userId);
 }
