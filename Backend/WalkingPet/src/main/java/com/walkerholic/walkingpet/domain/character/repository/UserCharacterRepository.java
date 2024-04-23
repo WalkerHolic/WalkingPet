63f360e5746026a2c613561fbedc38cb89aa5075
임현승 <lhs2689@naver.com>
2024-04-23 17:02:44 +0900

    [BE]Feat: UserCharacterRepository 메소드 추가(users로 UserChracter 찾기)

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/repository/UserCharacterRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/repository/UserCharacterRepository.java
@@ -1,10 +1,13 @@
 package com.walkerholic.walkingpet.domain.character.repository;
 
 import com.walkerholic.walkingpet.domain.character.entity.UserCharacter;
+import com.walkerholic.walkingpet.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserCharacterRepository extends JpaRepository<UserCharacter, Integer> {
     Optional<UserCharacter> findByUserCharacterId(int userCharacterId);
+
+    Optional<UserCharacter> findUserCharacterByUser(Users users);
 }
