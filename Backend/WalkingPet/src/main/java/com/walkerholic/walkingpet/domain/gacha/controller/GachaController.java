d2c4029a94c7d0055c0eff976399b336beb28567
정연우 <yoanwoo@naver.com>
2024-04-23 23:47:57 +0900

    [BE]feat : 뽑기 결과 컨트롤러 구현

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/gacha/controller/GachaController.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/gacha/controller/GachaController.java
@@ -1,6 +1,7 @@
 package com.walkerholic.walkingpet.domain.gacha.controller;
 
 import com.walkerholic.walkingpet.domain.gacha.dto.response.GachaCountResponse;
+import com.walkerholic.walkingpet.domain.gacha.dto.response.GachaResultResponse;
 import com.walkerholic.walkingpet.domain.gacha.service.GachaService;
 import com.walkerholic.walkingpet.global.error.GlobalSuccessCode;
 import com.walkerholic.walkingpet.global.error.response.CommonResponseEntity;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +21,13 @@ public class GachaController {
 
     @GetMapping("/count")
     public ResponseEntity<CommonResponseEntity> getGachaCount(){
-        GachaCountResponse gachaCount = gachaService.getGachaCount(1);
+        GachaCountResponse gachaCount = gachaService.getGachaCount(2);
         return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, gachaCount);
     }
 
+    @GetMapping("/result")
+    public ResponseEntity<CommonResponseEntity> getGachaResult(@RequestParam("box") String boxType){
+        GachaResultResponse gachaResult = gachaService.getGachaResult(boxType, 1);
+        return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS,gachaResult);
+    }
 }
