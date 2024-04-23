19435e5f9e7dd6421c653be596a5d20382b7afde
임현승 <lhs2689@naver.com>
2024-04-23 16:17:56 +0900

    Feat: Battle 관련 ResponseDTO 추가

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/battle/dto/response/UserBattleInfo.java
@@ -0,0 +1,29 @@
+package com.walkerholic.walkingpet.domain.battle.dto.response;
+
+import com.walkerholic.walkingpet.domain.character.entity.UserCharacter;
+import com.walkerholic.walkingpet.domain.users.entity.UserDetail;
+import lombok.*;
+
+@Builder
+@ToString
+@Getter
+public class UserBattleInfo {
+    private String nickname;
+    private int characterId;
+    private int health;
+    private int power;
+    private int defense;
+    private int rating;
+    private int battleCount;
+
+    public static UserBattleInfo from(UserCharacter userCharacter, UserDetail userDetail){
+        return UserBattleInfo.builder()
+                .nickname(userDetail.getUser().getNickname())
+                .characterId(userCharacter.getUserCharacterId())
+                .health(userCharacter.getHealth())
+                .power(userCharacter.getPower())
+                .defense(userCharacter.getDefense())
+                .battleCount(userDetail.getBattleCount())
+                .build();
+    }
+}
