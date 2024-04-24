e26a6a14d4bbd786f2eadab7daf8a3d96d0a2287
정연우 <yoanwoo@naver.com>
2024-04-24 13:45:54 +0900

    [BE]feat : TeamService 내 getUserTeams 메소드 구현
    사용자가 가입한 그룹들만 불러오는 getUserTeams를 구현하였다. 또한 기존의 getAllTeam 과 중복되는 코드가 있어 그 부분을 getTeamResponses 메소드로 따로 추출하였다.

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/service/TeamService.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/service/TeamService.java
@@ -1,9 +1,11 @@
 package com.walkerholic.walkingpet.domain.team.service;
 
-import com.walkerholic.walkingpet.domain.team.dto.AllTeamResponse;
+import com.walkerholic.walkingpet.domain.team.dto.TeamResponse;
 import com.walkerholic.walkingpet.domain.team.entity.Team;
 import com.walkerholic.walkingpet.domain.team.repository.TeamRepository;
 import com.walkerholic.walkingpet.domain.team.repository.TeamUserRepository;
+import com.walkerholic.walkingpet.domain.users.entity.Users;
+import com.walkerholic.walkingpet.domain.users.repository.UsersRepository;
 import com.walkerholic.walkingpet.global.error.GlobalBaseException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +14,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.walkerholic.walkingpet.global.error.GlobalErrorCode.TEAM_NOT_FOUND;
+import static com.walkerholic.walkingpet.global.error.GlobalErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -22,21 +26,36 @@ public class TeamService {
 
     private final TeamRepository teamRepository;
     private final TeamUserRepository teamUserRepository;
+    private final UsersRepository usersRepository;
 
     @Transactional
-    public List<AllTeamResponse> getAllTeam() {
+    public List<TeamResponse> getAllTeam() {
 
-            List<Team> teams = teamRepository.findAll();
-            List<AllTeamResponse> teamResponses = new ArrayList<>();
+        List<Team> teams = teamRepository.findAll();
+        return getTeamResponses(teams);
 
-            for (Team team : teams) {
-                Integer userCount = teamUserRepository.countByTeamId(team.getTeamId())
-                        .orElseThrow(() -> new GlobalBaseException(TEAM_NOT_FOUND));
-                AllTeamResponse teamResponse = AllTeamResponse.from(team, userCount);
-                teamResponses.add(teamResponse);
-            }
+    }
 
-            return teamResponses;
+    @Transactional
+    public List<TeamResponse> getUserTeams(int userId) {
 
+        Users user = usersRepository.findUsersByUserId(userId)
+                .orElseThrow(() -> new GlobalBaseException(USER_NOT_FOUND));
+
+        List<Team> teams = teamUserRepository.findTeamsByUser(user)
+                .orElseThrow(() -> new GlobalBaseException(TEAM_NOT_FOUND));
+
+        return getTeamResponses(teams);
+
+    }
+
+    private List<TeamResponse> getTeamResponses(List<Team> teams) {
+        return teams.stream()
+                .map(team -> {
+                    Integer userCount = teamUserRepository.countByTeamId(team.getTeamId())
+                            .orElseThrow(() -> new GlobalBaseException(TEAM_NOT_FOUND));
+                    return TeamResponse.from(team, userCount);
+                })
+                .collect(Collectors.toList());
     }
 }
