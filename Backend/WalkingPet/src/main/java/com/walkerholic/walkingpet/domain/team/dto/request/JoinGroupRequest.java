e5bb2eb8aca048a29f4a1ac93617c421705205cf
정연우 <yoanwoo@naver.com>
2024-04-25 00:59:02 +0900

    [BE]feat : 그룹 가입에 필요한 request Dto 작성

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/dto/request/JoinGroupRequest.java
@@ -0,0 +1,15 @@
+package com.walkerholic.walkingpet.domain.team.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@ToString
+@Getter
+public class JoinGroupRequest {
+
+    private Integer teamId;
+    private String password;
+
+}
