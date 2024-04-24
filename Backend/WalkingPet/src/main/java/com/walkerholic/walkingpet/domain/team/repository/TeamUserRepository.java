81b35991f63da23dd77abd765a2b97d6fca8d5dc
정연우 <yoanwoo@naver.com>
2024-04-24 13:43:34 +0900

    [BE]feat : TeamUserRepository에 findTeamsByUser 메소드 추가

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/repository/TeamUserRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/repository/TeamUserRepository.java
@@ -1,13 +1,19 @@
 package com.walkerholic.walkingpet.domain.team.repository;
 
+import com.walkerholic.walkingpet.domain.team.entity.Team;
 import com.walkerholic.walkingpet.domain.team.entity.TeamUser;
+import com.walkerholic.walkingpet.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamUserRepository extends JpaRepository<TeamUser, Integer> {
     @Query("SELECT COUNT(tu) FROM TeamUser tu WHERE tu.team.teamId = :teamId")
     Optional<Integer> countByTeamId(@Param("teamId") Integer teamId);
+
+    @Query("SELECT tu.team FROM TeamUser tu WHERE tu.user = :user")
+    Optional<List<Team>> findTeamsByUser(@Param("user") Users user);
 }
