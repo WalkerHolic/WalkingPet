df3c9eea31911a3400990695d4832ff109462c5f
임현승 <lhs2689@naver.com>
2024-04-25 20:47:28 +0900

    [BE]Feat: Battle Reward 관련한 UserItemRepository 메소드 추가(유저, 아이템 이름으로 UserItem 추가)

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/item/repository/UserItemRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/item/repository/UserItemRepository.java
@@ -11,4 +11,7 @@ public interface UserItemRepository extends JpaRepository<UserItem, Long> {
 
     @Query("SELECT ui FROM UserItem ui JOIN FETCH ui.user WHERE ui.user.userId = :userId")
     Optional<List<UserItem>> findByUserIdWithUserFetch(Integer userId);
+
+    @Query("select ui from UserItem ui join fetch ui.item where ui.user.userId = :userId and ui.item.name = :itemName")
+    Optional<UserItem> findByUserItemWithUserAndItemFetch(Integer userId, String itemName);
 }
