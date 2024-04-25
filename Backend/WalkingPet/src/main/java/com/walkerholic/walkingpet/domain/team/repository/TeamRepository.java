18ea35dd58a60ef812fef09d17dfb00bbc912f08
정연우 <yoanwoo@naver.com>
2024-04-25 10:28:09 +0900

    [BE]fix : findByNameContaining 반환 타입 수정

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/repository/TeamRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/team/repository/TeamRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Integer> {
-    Optional<List<Team>> findByNameContaining(String name);
+    List<Team> findByNameContaining(String name);
 
     Optional<Team> findByTeamId(Integer teamId);
 }
