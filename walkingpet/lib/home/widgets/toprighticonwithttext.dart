f2f343a8259b4bf30cc6743d70e8959df8c43c8d
여진구 <dmdkvj369@naver.com>
2024-04-25 15:06:50 +0900

    [FE] Feat: 메인 우측상단바 아이콘 모듈화

---
+++ walkingpet/lib/home/widgets/toprighticonwithttext.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class TopRightIconWithText extends StatelessWidget {
+  final String icon;
+  final String text;
+
+  const TopRightIconWithText({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/$icon');
+          },
+          icon: SvgPicture.asset(
+            'assets/icons/$icon.svg',
+            width: 60,
+            height: 60,
+          ),
+        ),
+        Transform.translate(
+          offset: const Offset(0, -10),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontFamily: "DungGeunMo",
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  // 외곽선 색상 및 오프셋 설정
+                  offset: Offset(-1.5, -1.5),
+                  color: Colors.black,
+                  blurRadius: 1,
+                ),
+                Shadow(
+                  offset: Offset(1.5, -1.5),
+                  color: Colors.black,
+                  blurRadius: 1,
+                ),
+                Shadow(
+                  offset: Offset(1.5, 1.5),
+                  color: Colors.black,
+                  blurRadius: 1,
+                ),
+                Shadow(
+                  offset: Offset(-1.5, 1.5),
+                  color: Colors.black,
+                  blurRadius: 1,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
