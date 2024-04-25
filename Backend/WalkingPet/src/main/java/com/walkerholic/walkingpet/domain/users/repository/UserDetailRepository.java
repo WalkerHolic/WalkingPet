f9a1bce458b05e3466f4d861cc95e674c339dd56
정연우 <yoanwoo@naver.com>
2024-04-25 15:49:17 +0900

    [BE]Feat : TeamUsersResponse DTO 구현

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/users/repository/UserDetailRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/users/repository/UserDetailRepository.java
@@ -1,12 +1,18 @@
 package com.walkerholic.walkingpet.domain.users.repository;
 
+import com.walkerholic.walkingpet.domain.team.entity.Team;
+import com.walkerholic.walkingpet.domain.team.entity.TeamUser;
 import com.walkerholic.walkingpet.domain.users.entity.UserDetail;
 import com.walkerholic.walkingpet.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface UserDetailRepository extends JpaRepository<UserDetail, Integer> {
     Optional<UserDetail> findUserDetailByUser(Users user);
+
 }
