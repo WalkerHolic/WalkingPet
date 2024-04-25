87b4252304c1855b2b5df046f4bc4be3a7f5464a
여진구 <dmdkvj369@naver.com>
2024-04-25 22:32:55 +0900

    [FE] Feat: 하단 nav바 디자인 및 기능 구현

--- walkingpet/lib/common/bottom_nav_bar.dart
+++ walkingpet/lib/common/bottom_nav_bar.dart
@@ -3,61 +3,83 @@ import 'package:flutter/material.dart';
 class BottomNavBar extends StatelessWidget {
   final int selectedIndex;
 
-  const BottomNavBar({
-    super.key,
-    required this.selectedIndex,
-  });
+  const BottomNavBar({super.key, required this.selectedIndex});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: selectedIndex,
-      backgroundColor: Colors.black, // 배경색을 검정색으로 설정
-      unselectedItemColor: Colors.white, // 선택되지 않은 아이템 색상을 흰색으로 설정
-      selectedItemColor: Colors.orange, // 선택된 아이템 색상을 주황색으로 설정
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.pets),
-          label: '캐릭터',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.star),
-          label: '뽑기',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: '홈',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.gamepad),
-          label: '배틀',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.pages),
-          label: '그룹',
-        ),
-      ],
-      onTap: (index) {
-        if (index != selectedIndex) {
-          switch (index) {
-            case 0:
-              Navigator.pushReplacementNamed(context, '/characterinfo');
-              break;
-            case 1:
-              Navigator.pushReplacementNamed(context, '/gacha');
-              break;
-            case 2:
-              Navigator.pushReplacementNamed(context, '/home');
-              break;
-            case 3:
-              Navigator.pushReplacementNamed(context, '/battle');
-              break;
-            case 4:
-              Navigator.pushReplacementNamed(context, '/group');
-              break;
-          }
-        }
-      },
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      color: const Color.fromRGBO(255, 255, 255, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          navBarItem(context, Icons.pets, '캐릭터', 0),
+          verticalDivider(),
+          navBarItem(context, Icons.star, '뽑기', 1),
+          verticalDivider(),
+          navBarItem(context, Icons.home, '홈', 2),
+          verticalDivider(),
+          navBarItem(context, Icons.gamepad, '배틀', 3),
+          verticalDivider(),
+          navBarItem(context, Icons.group, '그룹', 4),
+        ],
+      ),
     );
+  }
+
+  Widget navBarItem(
+      BuildContext context, IconData icon, String label, int index) {
+    return InkWell(
+      onTap: () {
+        // Navigate
+        _onItemTapped(context, index);
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon,
+              size: 30,
+              color: selectedIndex == index
+                  ? const Color.fromRGBO(3, 169, 244, 1)
+                  : const Color.fromRGBO(214, 214, 214, 1)),
+          Text(
+            label,
+            style: TextStyle(
+                color: selectedIndex == index
+                    ? const Color.fromRGBO(3, 169, 244, 1)
+                    : const Color.fromRGBO(214, 214, 214, 1)),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget verticalDivider() {
+    return Container(
+      height: 30,
+      width: 1,
+      color: Colors.grey,
+    );
+  }
+}
+
+// 이 함수 내에 네비게이션 로직 추가
+void _onItemTapped(BuildContext context, int index) {
+  switch (index) {
+    case 0:
+      Navigator.pushReplacementNamed(context, '/characterinfo');
+      break;
+    case 1:
+      Navigator.pushReplacementNamed(context, '/gacha');
+      break;
+    case 2:
+      Navigator.pushReplacementNamed(context, '/home');
+      break;
+    case 3:
+      Navigator.pushReplacementNamed(context, '/battle');
+      break;
+    case 4:
+      Navigator.pushReplacementNamed(context, '/group');
+      break;
   }
 }
