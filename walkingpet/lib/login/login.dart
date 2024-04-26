f8b9282cc377d6dbdc193df5703d21f2fed96726
정연우 <yoanwoo@naver.com>
2024-04-26 12:32:24 +0900

    [FE]Feat: 로그인 페이지 퍼블리싱 완료

--- walkingpet/lib/login/login.dart
+++ walkingpet/lib/login/login.dart
@@ -14,14 +14,63 @@ class Login extends StatelessWidget {
             fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
           ),
         ),
-        child: const Center(
+        child: Center(
           child: Column(
             children: [
+              const SizedBox(
+                height: 120,
+              ),
               // 워킹펫 문구 크게
+              const Text(
+                "Walking Pet",
+                style: TextStyle(
+                  fontSize: 52,
+                  color: Color.fromRGBO(141, 198, 63, 1),
+                  shadows: [
+                    Shadow(
+                      // 외곽선 색상 및 오프셋 설정
+                      offset: Offset(-1.5, -1.5),
+                      color: Color.fromRGBO(54, 91, 18, 1),
+                      blurRadius: 1,
+                    ),
+                    Shadow(
+                      offset: Offset(1.5, -1.5),
+                      color: Color.fromRGBO(54, 91, 18, 1),
+                      blurRadius: 1,
+                    ),
+                    Shadow(
+                      offset: Offset(1.5, 1.5),
+                      color: Color.fromRGBO(54, 91, 18, 1),
+                      blurRadius: 1,
+                    ),
+                    Shadow(
+                      offset: Offset(-1.5, 1.5),
+                      color: Color.fromRGBO(54, 91, 18, 1),
+                      blurRadius: 1,
+                    ),
+                  ],
+                ),
+              ),
 
+              const SizedBox(
+                height: 220,
+              ),
               // 걷는 캐릭터
+              Image.asset(
+                'assets/animals/cow/cow_run.gif',
+              ),
 
-              // 카카오 로그인
+              const SizedBox(
+                height: 40,
+              ),
+
+              InkWell(
+                onTap: () {
+                  // 네비게이션 함수 호출 예시
+                  Navigator.pushNamed(context, '/home'); // 로그인 후 이동할 페이지 경로 지정
+                },
+                child: Image.asset('assets/icons/kakao_login_medium_wide.png'),
+              ),
             ],
           ),
         ),
