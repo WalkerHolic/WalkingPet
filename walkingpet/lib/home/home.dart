206d3c2fb479dee290d8168f3ed3577053172e3c
신유진 <yujin1698@naver.com>
2024-04-26 13:15:10 +0900

    [FE]Fix: login 충돌 해결

--- walkingpet/lib/home/home.dart
+++ walkingpet/lib/home/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:walkingpet/common/bottom_nav_bar.dart';
 import 'package:walkingpet/home/widgets/mainfontstyle.dart';
 import 'package:walkingpet/home/widgets/toprighticonwithttext.dart';
-import 'package:walkingpet/home/widgets/mainfontstyle.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,47 +9,56 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/backgrounds/day.png"), // 이미지 파일 경로 지정
-            fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
-          ),
-        ),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 30,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/backgrounds/day.png"), // 이미지 파일 경로 지정
+                fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+            ),
+            child: Center(
+              child: Column(
                 children: [
-                  Transform.translate(
-                    offset: const Offset(10, 0),
-                    child:
-                        const TopRightIconWithText(icon: "ranking", text: "랭킹"),
+                  const SizedBox(
+                    height: 30,
                   ),
-                  const TopRightIconWithText(icon: "goal", text: "목표"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Transform.translate(
+                        offset: const Offset(10, 0),
+                        child: const TopRightIconWithText(
+                            icon: "ranking", text: "랭킹"),
+                      ),
+                      const TopRightIconWithText(icon: "goal", text: "목표"),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const MainFontStyle(size: 48, text: "걸음수"),
+                  Transform.translate(
+                    offset: const Offset(0, -20),
+                    child: const MainFontStyle(size: 100, text: "16384"),
+                  ),
+                  Image.asset(
+                    'assets/animals/cow/cow_run.gif',
+                  ),
                 ],
               ),
-              const SizedBox(
-                height: 40,
-              ),
-              const MainFontStyle(size: 48, text: "걸음수"),
-              Transform.translate(
-                offset: const Offset(0, -20),
-                child: const MainFontStyle(size: 100, text: "16384"),
-              ),
-              Image.asset(
-                'assets/animals/cow/cow_run.gif',
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-      bottomNavigationBar: const BottomNavBar(
-        selectedIndex: 2,
+          const Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: BottomNavBar(
+              selectedIndex: 2,
+            ),
+          ),
+        ],
       ),
     );
   }
