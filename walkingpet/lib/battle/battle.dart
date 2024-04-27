787934f86053bc0662b2f8dfb00af35455de7ea9
신유진 <yujin1698@naver.com>
2024-04-27 19:59:43 +0900

    [FE] Feat: 걸음수 관리 파일 생성 및 스카폴드, 컨테이너 위치 전환

--- walkingpet/lib/battle/battle.dart
+++ walkingpet/lib/battle/battle.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:walkingpet/common/bottom_nav_bar.dart';
+import 'package:walkingpet/home/widgets/mainfontstyle.dart';
 
 class Battle extends StatelessWidget {
   const Battle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('배틀 Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/home');
-          },
-          child: const Text('Go Back to Home Page'),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image:
+              AssetImage("assets/backgrounds/battleReady.jpg"), // 이미지 파일 경로 지정
+          fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
         ),
       ),
-      bottomNavigationBar: const BottomNavBar(
-        selectedIndex: 3,
+      child: Scaffold(
+        backgroundColor: Colors.transparent, // Scaffold의 배경을 투명하게 설정
+        body: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(
+                  20.0, 40.0, 20.0, 100.0), // 좌, 상, 우, 하 각각 다른 마진 적용
+
+              padding: const EdgeInsets.all(16), // 모든 방향에 16.0의 패딩을 적용
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.6), // 투명한 흰색 배경 추가
+                borderRadius: BorderRadius.circular(10.0), // 모서리를 둥글게 처리
+              ),
+              child: const Center(
+                child: Column(
+                  children: [
+                    MainFontStyle(size: 40, text: "오리빠따죠"),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        MainFontStyle(size: 30, text: "점수:"),
+                        MainFontStyle(size: 50, text: "1357")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: BottomNavBar(
+                selectedIndex: 3,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- walkingpet/lib/home/home.dart
+++ walkingpet/lib/home/home.dart
@@ -1,64 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:walkingpet/common/bottom_nav_bar.dart';
+import 'package:walkingpet/common/step_counter.dart';
 import 'package:walkingpet/home/widgets/mainfontstyle.dart';
 import 'package:walkingpet/home/widgets/toprighticonwithttext.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/backgrounds/day.png"), // 이미지 파일 경로 지정
-                fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/backgrounds/day.png"), // 이미지 파일 경로 지정
+          fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
+        ),
+      ),
+      child: ChangeNotifierProvider(
+        create: (context) => StepCounter(),
+        child: Scaffold(
+          backgroundColor: Colors.transparent, // Scaffold 배경을 투명하게 설정
+          body: Stack(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Transform.translate(
+                          offset: const Offset(10, 0),
+                          child: const TopRightIconWithText(
+                              icon: "ranking", text: "랭킹"),
+                        ),
+                        const TopRightIconWithText(icon: "goal", text: "목표"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    const MainFontStyle(size: 48, text: "걸음수"),
+                    Transform.translate(
+                      offset: const Offset(0, -20),
+                      child: Consumer<StepCounter>(
+                          builder: (context, provider, child) {
+                        return MainFontStyle(
+                            size: 100, text: "${provider.steps}");
+                      }),
+                    ),
+                    Image.asset(
+                      'assets/animals/cow/cow_run.gif',
+                    ),
+                  ],
+                ),
               ),
-            ),
-            child: Center(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Transform.translate(
-                        offset: const Offset(10, 0),
-                        child: const TopRightIconWithText(
-                            icon: "ranking", text: "랭킹"),
-                      ),
-                      const TopRightIconWithText(icon: "goal", text: "목표"),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  const MainFontStyle(size: 48, text: "걸음수"),
-                  Transform.translate(
-                    offset: const Offset(0, -20),
-                    child: const MainFontStyle(size: 100, text: "16384"),
-                  ),
-                  Image.asset(
-                    'assets/animals/cow/cow_run.gif',
-                  ),
-                ],
+              const Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: BottomNavBar(
+                  selectedIndex: 2,
+                ),
               ),
-            ),
+            ],
           ),
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: BottomNavBar(
-              selectedIndex: 2,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
