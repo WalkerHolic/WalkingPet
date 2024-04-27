348c570f7d93c9579616882c94ff5a91ceb8db10
이지은 <lju0514@naver.com>
2024-04-27 17:02:53 +0900

    [FE] Feat: 랭킹페이지 Top10 스크롤 기능 구현

--- walkingpet/lib/ranking/widgets/personal.dart
+++ walkingpet/lib/ranking/widgets/personal.dart
@@ -35,16 +35,16 @@ class PersonalRanking extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Top1to3(
-                ranking: '1',
-                characterId: '13',
-                nickname: '1등 야호',
-                step: '362,254',
-              ),
-              Top1to3(
                 ranking: '2',
                 characterId: '13',
                 nickname: '이겜재밌냐',
                 step: '226,254',
+              ),
+              Top1to3(
+                ranking: '1',
+                characterId: '13',
+                nickname: '1등 야호',
+                step: '362,254',
               ),
               Top1to3(
                 ranking: '3',
@@ -72,40 +72,76 @@ class PersonalRanking extends StatelessWidget {
             style: TextStyle(fontSize: 30),
           ),
 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Rank(
-                ranking: '1',
-                characterId: '13',
-                nickname: '1등 야호',
-                step: '362,254',
+          SizedBox(
+            height: 230,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Rank(
+                    ranking: '1',
+                    characterId: '13',
+                    nickname: '1등 야호',
+                    step: '362,254',
+                  ),
+                  Rank(
+                    ranking: '2',
+                    characterId: '13',
+                    nickname: '이겜재밌냐',
+                    step: '226,254',
+                  ),
+                  Rank(
+                    ranking: '3',
+                    characterId: '13',
+                    nickname: '나는지은',
+                    step: '160,254',
+                  ),
+                  Rank(
+                    ranking: '4',
+                    characterId: '1',
+                    nickname: '룰루루4등잼',
+                    step: '8,254',
+                  ),
+                  Rank(
+                    ranking: '5',
+                    characterId: '1',
+                    nickname: '우희힇',
+                    step: '7,254',
+                  ),
+                  Rank(
+                    ranking: '6',
+                    characterId: '1',
+                    nickname: '6등 울렐레',
+                    step: '6,254',
+                  ),
+                  Rank(
+                    ranking: '7',
+                    characterId: '1',
+                    nickname: '7등임당',
+                    step: '5,254',
+                  ),
+                  Rank(
+                    ranking: '8',
+                    characterId: '1',
+                    nickname: '난 8등',
+                    step: '3,254',
+                  ),
+                  Rank(
+                    ranking: '9',
+                    characterId: '1',
+                    nickname: '구구구구구',
+                    step: '2,254',
+                  ),
+                  Rank(
+                    ranking: '10',
+                    characterId: '1',
+                    nickname: '10등이군',
+                    step: '1,254',
+                  ),
+                ],
               ),
-              Rank(
-                ranking: '2',
-                characterId: '13',
-                nickname: '이겜재밌냐',
-                step: '226,254',
-              ),
-              Rank(
-                ranking: '3',
-                characterId: '13',
-                nickname: '나는지은',
-                step: '160,254',
-              ),
-              Rank(
-                ranking: '4',
-                characterId: '1',
-                nickname: '룰루루4등잼',
-                step: '6,254',
-              ),
-              Rank(
-                ranking: '5',
-                characterId: '1',
-                nickname: '우희힇',
-                step: '1,254',
-              ),
-            ],
+            ),
           ),
 
           // 5. 아래 빈 공간
