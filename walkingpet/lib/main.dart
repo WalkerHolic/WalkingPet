3d5881bf87728b05dea6f3dc028aab3a5b9677df
신유진 <yujin1698@naver.com>
2024-04-27 19:59:43 +0900

    [FE] Feat: 걸음수 관리 파일 생성 및 스카폴드, 컨테이너 위치 전환

--- walkingpet/lib/main.dart
+++ walkingpet/lib/main.dart
@@ -9,10 +9,25 @@ import 'package:walkingpet/login/login.dart';
 import 'package:walkingpet/ranking/ranking.dart';
 // import 'package:nes_ui/nes_ui.dart';
 import 'package:flutter/services.dart'; // SystemChrome을 사용하기 위해 필요
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  //SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+  _requestPermissions();
+}
+
+// 권한 요청 메소드 정의
+void _requestPermissions() async {
+  // ACTIVITY_RECOGNITION 권한 요청
+  var status = await Permission.activityRecognition.request();
+  if (status.isGranted) {
+  } else {
+    // 사용자가 권한을 거부한 경우 처리할 로직 추가 가능
+    SystemNavigator.pop();
+  }
 }
 
 class MyApp extends StatelessWidget {
