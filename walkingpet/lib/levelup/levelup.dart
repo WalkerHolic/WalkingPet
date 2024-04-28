e5e533fd62a55626186c065506274d246ecc59d5
이지은 <lju0514@naver.com>
2024-04-29 02:07:14 +0900

    [FE] Feat: 레벨업 페이지 생성

---
+++ walkingpet/lib/levelup/levelup.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LevelUp extends StatelessWidget {
+  const LevelUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('레벨업 페이지')),
+    );
+  }
+}
