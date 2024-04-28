247f5661761966459370f033e8f1fb93a327039a
임현승 <lhs2689@naver.com>
2024-04-29 03:40:29 +0900

    [BE]Feat: test용 LevelUpController 추가

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/levelup/controller/LevelUpController.java
@@ -0,0 +1,28 @@
+package com.walkerholic.walkingpet.domain.levelup.controller;
+
+import com.walkerholic.walkingpet.domain.levelup.dto.response.LevelUpResponse;
+import com.walkerholic.walkingpet.domain.levelup.service.LevelUpService;
+import com.walkerholic.walkingpet.global.error.GlobalSuccessCode;
+import com.walkerholic.walkingpet.global.error.response.CommonResponseEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/levelup")
+public class LevelUpController {
+    private final LevelUpService levelUpService;
+
+    @GetMapping("/test")
+    public ResponseEntity<CommonResponseEntity> levelUpTest(@RequestParam("userId") int userId, @RequestParam("getExp") int getExp){
+        LevelUpResponse levelUpResponse = levelUpService.getLevelUpResponse(userId,getExp);
+
+        return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, levelUpResponse);
+    }
+}
