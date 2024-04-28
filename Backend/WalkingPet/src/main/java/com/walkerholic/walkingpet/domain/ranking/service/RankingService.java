c1ea295871f245e433ea0f4bcd58f5f01e43f775
신유진 <yujin1698@naver.com>
2024-04-28 18:23:42 +0900

    [BE]Feat: 개인 랭킹 누적 기준 유저 순위 조회

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/ranking/service/RankingService.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/ranking/service/RankingService.java
@@ -2,9 +2,14 @@ package com.walkerholic.walkingpet.domain.ranking.service;
 
 import com.walkerholic.walkingpet.domain.ranking.dto.AccStepTop10Ranking;
 import com.walkerholic.walkingpet.domain.ranking.dto.AccStepTop3Ranking;
-import com.walkerholic.walkingpet.domain.ranking.dto.response.*;
+import com.walkerholic.walkingpet.domain.ranking.dto.response.AccStepTop10RankingResponse;
+import com.walkerholic.walkingpet.domain.ranking.dto.response.AccStepTop3RankingResponse;
+import com.walkerholic.walkingpet.domain.ranking.dto.response.PersonalStepRankingResponse;
+import com.walkerholic.walkingpet.domain.ranking.dto.response.UserPersonalStepRankingResponse;
 import com.walkerholic.walkingpet.domain.users.entity.UserStep;
 import com.walkerholic.walkingpet.domain.users.repository.UserStepRepository;
+import com.walkerholic.walkingpet.global.error.GlobalBaseException;
+import com.walkerholic.walkingpet.global.error.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -28,8 +33,9 @@ public class RankingService {
             accStepRankingList.add(AccStepTop10Ranking.from(userStep));
         }
 
-        UserPersonalStepRankingResponse userPersonalStepRanking = new UserPersonalStepRankingResponse();
-        return PersonalStepRankingResponse.from(userPersonalStepRanking, accStepRankingList);
+        int userId = 1;
+        UserPersonalStepRankingResponse userAccStepRanking = getUserAccStepRanking(1);
+        return PersonalStepRankingResponse.from(userAccStepRanking, accStepRankingList);
     }
 
     /**
@@ -48,7 +54,7 @@ public class RankingService {
     }
 
     /**
-     * 누적 걸음수 기준으로 top 3 랭킹 가져오기
+     * 누적 걸음수 기준으로 top 3 개인 랭킹 가져오기
      */
     public AccStepTop3RankingResponse getAccStepRankingTop3() {
         //TODO: Redis 데이터로 변경
@@ -60,5 +66,17 @@ public class RankingService {
         }
 
         return AccStepTop3RankingResponse.from(accStepRankingList);
+    }
+
+    /**
+     * 누적 걸음수 기준으로 개인 랭킹에서 해당 유저의 랭킹 가져오기
+     */
+    public UserPersonalStepRankingResponse getUserAccStepRanking(int userId) {
+        //TODO: Redis 데이터로 변경
+        int userRanking = userStepRepository.findUserPersonalRankingByAccStep(userId);
+        UserStep userStep = userStepRepository.findUserStepByUserUserId(userId)
+                .orElseThrow(() -> new GlobalBaseException(GlobalErrorCode.USER_STEP_NOT_FOUND));
+
+        return UserPersonalStepRankingResponse.from(userRanking, userStep.getAccumulationStep());
     }
 }
--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/users/repository/UserStepRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/users/repository/UserStepRepository.java
@@ -3,8 +3,10 @@ package com.walkerholic.walkingpet.domain.users.repository;
 import com.walkerholic.walkingpet.domain.users.entity.UserStep;
 import com.walkerholic.walkingpet.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public interface UserStepRepository extends JpaRepository<UserStep, Integer> {
@@ -13,4 +15,18 @@ public interface UserStepRepository extends JpaRepository<UserStep, Integer> {
     Optional<UserStep> findUserStepByUserUserId(Integer userId);
 
     List<UserStep> findTop10ByOrderByAccumulationStepDesc();
+
+    List<UserStep> findByOrderByAccumulationStepDesc();
+
+    @Query("""
+        SELECT COUNT(*) + 1
+        FROM UserStep US1
+        WHERE US1.accumulationStep >
+        (
+            SELECT US2.accumulationStep
+            FROM UserStep US2
+            WHERE US2.user.userId = :userId
+        )
+        """)
+    int findUserPersonalRankingByAccStep(Integer userId);
 }
