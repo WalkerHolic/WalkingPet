3563fda010be2cf8b6ae0ecd130081be4b0f19c3
이지은 <lju0514@naver.com>
2024-04-29 02:43:45 +0900

    [FE] Feat: 레벨업 페이지 퍼블리싱

--- walkingpet/lib/character/characterinfo.dart
+++ walkingpet/lib/character/characterinfo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:walkingpet/character/widgets/character_stat.dart';
 import 'package:walkingpet/common/bottom_nav_bar.dart';
 
