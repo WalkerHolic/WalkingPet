9f139d99a817a39d59056a87fae4e3d2bc2bf39d
임현승 <lhs2689@naver.com>
2024-04-29 01:28:23 +0900

    [BE]Feat: userId로 UserCharacter 찾는 메소드 추가

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/repository/UserCharacterRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/repository/UserCharacterRepository.java
@@ -4,6 +4,7 @@ import com.walkerholic.walkingpet.domain.character.entity.Character;
 import com.walkerholic.walkingpet.domain.character.entity.UserCharacter;
 import com.walkerholic.walkingpet.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,4 +12,7 @@ public interface UserCharacterRepository extends JpaRepository<UserCharacter, In
     Optional<UserCharacter> findByUserCharacterId(int userCharacterId);
 
     Optional<UserCharacter> findByUserAndCharacter(Users user, Character character);
+
+    @Query("select uc from UserCharacter uc where uc.user.userId = :userId")
+    Optional<UserCharacter> findUserCharacterByUserId(int userId);
 }
