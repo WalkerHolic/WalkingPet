6fee7830a02a4cc6707f8dd75df3eac4a8d99b27
임현승 <lhs2689@naver.com>
2024-04-29 12:25:25 +0900

    [BE]Fix: LevelUpFunction 에러 수정(HashMap contains 수정)

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/levelup/function/LevelUpFunction.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/levelup/function/LevelUpFunction.java
@@ -11,9 +11,15 @@ import java.util.HashMap;
 @Component
 @RequiredArgsConstructor
 public class LevelUpFunction {
+    static final int LEVEL_UP_GET_STAT_POINT = 5;
     private final UserCharacterFunction userCharacterFunction;
 
-    //레벨업을 할 수 있는지 체크해주는 함수.
+    /**
+     * 레벨업을 할 수 있는지 체크하는 함수
+     * @param requiredCheckLevelUpInfo 현재레벨과 경험치를 담고있는 객체
+     * @param getExperience 여러 요소를 통해 얻은 경험치 양
+     * @return 레벨업을 할 수 있다면 true, 할 수 없다면 false
+     */
     public boolean checkLevelUp(CharacterLevelExperienceInfo requiredCheckLevelUpInfo, int getExperience){
         int requiredExperience = userCharacterFunction.getMaxExperience(requiredCheckLevelUpInfo.getNowLevel());
 
@@ -23,7 +29,12 @@ public class LevelUpFunction {
         else return false;
     }
 
-    //레벨업을 했을 때 레벨 몇까지 업하고 남는 경험치는 몇인지 알려주는 함수
+    /**
+     * 레벨업을 했을 때 레벨 몇까지 업하고 남는 경험치는 몇인지 알려주는 함수
+     * @param requiredCheckLevelUpInfo 현재레벨과 경험치를 담고있는 객체
+     * @param getExperience 여러 요소를 통해 얻은 경험치 양
+     * @return 레벨업 이후의 레벨과 경험치
+     */
     public CharacterLevelExperienceInfo getNextLevel(CharacterLevelExperienceInfo requiredCheckLevelUpInfo, int getExperience){
         //현재 레벨
         int nowLevel = requiredCheckLevelUpInfo.getNowLevel();
@@ -48,32 +59,36 @@ public class LevelUpFunction {
     }
 
     //레벨업이 되었을 때 얻게되는 보상의 정보
+
+    /**
+     * @return 레벨업으로 받게 되는 스탯포인트, 얻게되는 상자와 수량
+     */
     public LevelUpReward getLevelUpReward(int nowLevel, int nextLevel){
         HashMap<String, Integer> itemReward = new HashMap<>();
 
         for(int i = nowLevel+1; i <= nextLevel ; i++){
             //5레벨 배수마다 고급상자 1개 지급
             if(i % 5 == 0){
-                if(itemReward.containsKey("고급상자"))
+                if(!itemReward.containsKey("고급상자"))
                     itemReward.put("고급상자", 1);
                 else{
-                    int nowRewardQuantity = itemReward.get("고급상자");
+                    Integer nowRewardQuantity = itemReward.get("고급상자");
                     itemReward.replace("고급상자", nowRewardQuantity+1);
                 }
             }
             //아니면 일반상자 1개 지급
             else{
-                if(itemReward.containsKey("일반상자"))
+                if(!itemReward.containsKey("일반상자"))
                     itemReward.put("일반상자", 1);
                 else{
-                    int nowRewardQuantity = itemReward.get("일반상자");
+                    Integer nowRewardQuantity = itemReward.get("일반상자");
                     itemReward.replace("일반상자", nowRewardQuantity+1);
                 }
             }
         }
 
         return LevelUpReward.builder()
-                .statPoint(nextLevel - nowLevel)
+                .statPoint((nextLevel - nowLevel)*LEVEL_UP_GET_STAT_POINT)
                 .itemReward(itemReward)
                 .build();
     }
