2122a65ef7c5f2e0526ac30e6eea939cec10d61c
이지은 <lju0514@naver.com>
2024-05-01 23:41:01 +0900

    [FE] Feat: 개인랭킹 페이지 Top10 api 연결 진행

--- walkingpet/lib/ranking/widgets/personal.dart
+++ walkingpet/lib/ranking/widgets/personal.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:walkingpet/ranking/widgets/rank.dart';
 import 'package:walkingpet/ranking/widgets/top1to3.dart';
+import 'package:walkingpet/services/ranking/ranking_api.dart';
 
 class PersonalRanking extends StatelessWidget {
-  const PersonalRanking({super.key});
+  PersonalRanking({super.key});
+
+  Future<List<Rank>> rankingPersonTop10Yesterday =
+      RankingApiService.getPersonTop10Yesterday();
 
   @override
   Widget build(BuildContext context) {
@@ -59,15 +63,24 @@ class PersonalRanking extends StatelessWidget {
           // 유진이 피드백: 나의 랭킹 897위 (226,254걸음 이런식으로 하면 어떨까?)
           const Padding(
             padding: EdgeInsets.symmetric(vertical: 20),
-            child: Rank(
-              ranking: '897',
-              characterId: '1',
-              nickname: '하이빅싸피',
-              step: '1,000',
-            ),
+            // child: Rank(
+            //   ranking: '897',
+            //   nickname: '하이빅싸피',
+            //   step: '1,000',
+            // ),
           ),
 
           // 4. 4~10위 표시
+          FutureBuilder(
+            future: rankingPersonTop10Yesterday,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return const Text("There is data!");
+              }
+              return const Text('랭킹 불러오는 중.. 기다려주세요 :D');
+            },
+          ),
+
           Container(
             // 4-1. 영역 표시
             decoration: BoxDecoration(
@@ -92,66 +105,57 @@ class PersonalRanking extends StatelessWidget {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Rank(
-                          ranking: '1',
-                          characterId: '13',
-                          nickname: '1등 야호',
-                          step: '362,254',
-                        ),
-                        Rank(
-                          ranking: '2',
-                          characterId: '13',
-                          nickname: '이겜재밌냐',
-                          step: '226,254',
-                        ),
-                        Rank(
-                          ranking: '3',
-                          characterId: '13',
-                          nickname: '나는지은',
-                          step: '160,254',
-                        ),
-                        Rank(
-                          ranking: '4',
-                          characterId: '1',
-                          nickname: '룰루루4등잼',
-                          step: '8,254',
-                        ),
-                        Rank(
-                          ranking: '5',
-                          characterId: '1',
-                          nickname: '우희힇',
-                          step: '7,254',
-                        ),
-                        Rank(
-                          ranking: '6',
-                          characterId: '1',
-                          nickname: '6등 울렐레',
-                          step: '6,254',
-                        ),
-                        Rank(
-                          ranking: '7',
-                          characterId: '1',
-                          nickname: '7등임당',
-                          step: '5,254',
-                        ),
-                        Rank(
-                          ranking: '8',
-                          characterId: '1',
-                          nickname: '난 8등',
-                          step: '3,254',
-                        ),
-                        Rank(
-                          ranking: '9',
-                          characterId: '1',
-                          nickname: '구구구구구',
-                          step: '2,254',
-                        ),
-                        Rank(
-                          ranking: '10',
-                          characterId: '1',
-                          nickname: '10등이군',
-                          step: '1,254',
-                        ),
+                        // Rank.fromJson(json),
+                        // Rank(
+                        //   ranking: '1',
+                        //   nickname: '1등 야호',
+                        //   step: '362,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '2',
+                        //   nickname: '이겜재밌냐',
+                        //   step: '226,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '3',
+                        //   nickname: '나는지은',
+                        //   step: '160,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '4',
+                        //   nickname: '룰루루4등잼',
+                        //   step: '8,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '5',
+                        //   nickname: '우희힇',
+                        //   step: '7,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '6',
+                        //   nickname: '6등 울렐레',
+                        //   step: '6,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '7',
+                        //   nickname: '7등임당',
+                        //   step: '5,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '8',
+                        //   nickname: '난 8등',
+                        //   step: '3,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '9',
+                        //   nickname: '구구구구구',
+                        //   step: '2,254',
+                        // ),
+                        // Rank(
+                        //   ranking: '10',
+                        //   nickname: '10등이군',
+                        //   step: '1,254',
+                        // ),
                       ],
                     ),
                   ),
