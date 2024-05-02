01ffb0a1615cd8aa9d1b2a4439e65d24ae24757c
임현승 <lhs2689@naver.com>
2024-05-03 00:18:44 +0900

    [BE] Feat: 회원가입할 때 테이블 초기화 테스트 Controller

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/users/controller/InItTableTestController.java
@@ -0,0 +1,32 @@
+package com.walkerholic.walkingpet.domain.users.controller;
+
+import com.walkerholic.walkingpet.domain.ranking.dto.response.PersonalStepRankingResponse;
+import com.walkerholic.walkingpet.domain.users.service.SignInService;
+import com.walkerholic.walkingpet.global.error.GlobalSuccessCode;
+import com.walkerholic.walkingpet.global.error.response.CommonResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/init")
+public class InItTableTestController {
+
+    private final SignInService signInService;
+    @GetMapping
+    public ResponseEntity<CommonResponseEntity> getPersonalRanking(@RequestParam("email") String email) {
+
+        signInService.signInInItTable(email);
+        return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, null    );
+    }
+}
