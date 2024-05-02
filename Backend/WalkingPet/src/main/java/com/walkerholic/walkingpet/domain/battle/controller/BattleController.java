3e0d9d9e007cf727a0717b3668003ead216c4553
임현승 <lhs2689@naver.com>
2024-05-02 16:39:52 +0900

    [BE] Fix: BattleController 수정

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/battle/controller/BattleController.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/battle/controller/BattleController.java
@@ -1,10 +1,8 @@
 package com.walkerholic.walkingpet.domain.battle.controller;
 
-import com.walkerholic.walkingpet.domain.battle.dto.response.BattleResponse;
-import com.walkerholic.walkingpet.domain.battle.dto.response.EnemyInfo;
+import com.walkerholic.walkingpet.domain.battle.dto.response.BattleResponseDTO;
 import com.walkerholic.walkingpet.domain.battle.dto.response.UserBattleInfoDTO;
 import com.walkerholic.walkingpet.domain.battle.service.BattleService;
-import com.walkerholic.walkingpet.domain.battle.service.TestBattleService;
 import com.walkerholic.walkingpet.global.error.GlobalSuccessCode;
 import com.walkerholic.walkingpet.global.error.response.CommonResponseEntity;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BattleController {
 
     private final BattleService battleService;
-    private final TestBattleService testBattleService;
 
     @GetMapping("/myinfo")
     @Operation(summary = "유저 배틀 정보 확인", description = "유저의 user,userDetail,userCharacter로 유저 배틀 정보 가져오기")
@@ -36,24 +32,12 @@ public class BattleController {
         return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, userBattleInfoDTO);
     }
 
-    @GetMapping("/enemyinfo")
-    @Operation(summary = "상대 유저 배틀 정보 확인", description = "상대 유저의 user,userDetail,userCharacter로 유저 배틀 정보 가져오기")
-    public ResponseEntity<CommonResponseEntity> getEnemyBattleInfo(){
-
-
-        int userId = 1;
-        log.info("적의 배틀 정보 확인하기 BattleController getEnemyBattleInfo - userId: {}", userId);
-        EnemyInfo enemyInfo = battleService.getEnemyBattleInfo(userId);
-
-        return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, enemyInfo);
-    }
-
     @GetMapping("/start")
     @Operation(summary = "배틀 전체 과정 출력", description = "배틀 시작, 진행 과정, 결과, 보상 출력")
     public ResponseEntity<CommonResponseEntity> battleStart(){
         int userId = 1;
         log.info("배틀 시작 BattleController battleStart - userId: {}", userId);
-        BattleResponse battleResponse = battleService.startBattle(userId);
+        BattleResponseDTO battleResponse = battleService.getBattleResponse(userId);
 
         return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, battleResponse);
     }
