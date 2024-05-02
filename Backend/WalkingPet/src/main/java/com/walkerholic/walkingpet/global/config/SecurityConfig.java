86abfc018c9f74050e37b81b5b5d0cef67519461
신유진 <yujin1698@naver.com>
2024-05-03 05:52:19 +0900

    [BE] Chore:security 설정 파일 및 swagger 설정 파일

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.walkerholic.walkingpet.global.config;
+
+import com.walkerholic.walkingpet.domain.auth.filter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    JwtAuthorizationFilter jwtAuthorizationFilter;
+
+
+    @Autowired
+    public SecurityConfig(JwtAuthorizationFilter jwtAuthorizationFilter) {
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+    }
+
+    // HTTP 요청에 대한 보안 필터 체인을 정의, 특정 URL 패턴 또는 요청에 대한 보안 필터의 집합을 정의
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated()
+
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout.logoutSuccessUrl("/").permitAll());
+
+
+        return http.build();
+    }
+
+
+    // Spring Security의 구성을 조정하고 사용자 지정
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers(
+//                "/swagger-ui/**",
+//                "/swagger-config",
+//                "/error"
+//        );
+//    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.authorizeRequests()
+//                .antMatchers("/**").authenticated();
+//    }
+}
