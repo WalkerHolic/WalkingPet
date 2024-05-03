21b1e4b47f44bfbbb8987349bb8e155d6dd0c49e
이지은 <lju0514@naver.com>
2024-05-04 01:02:13 +0900

    [FE] Fix: 캐릭터 변경 모달 오류 수정 및 캐릭터 gif 정리

--- walkingpet/lib/character/widgets/character_box.dart
+++ walkingpet/lib/character/widgets/character_box.dart
@@ -27,7 +27,7 @@ class CharacterBox extends StatelessWidget {
     // double screenHeight = MediaQuery.of(context).size.height;
 
     String animal = CharacterMap.idToAnimal[characterId] ?? "buddy";
-    print(animal);
+    // print(animal);
 
     return Container(
       width: screenWidth * 0.2,
@@ -87,8 +87,8 @@ class CharacterBox extends StatelessWidget {
 
               // 3. 캐릭터 이미지(id)
               Image.asset(
-                'assets/animals/cow/cow_idle.gif',
-                // 'assets/animals/$animal/${animal}_walk.gif',
+                // 'assets/animals/cow/cow_idle.gif',
+                'assets/animals/$animal/${animal}_idle.gif',
                 height: 70,
                 // scale: 1,
               ),
