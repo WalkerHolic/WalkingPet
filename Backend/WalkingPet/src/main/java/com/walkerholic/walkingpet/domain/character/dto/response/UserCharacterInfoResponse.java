c8fe5a108a58cc1c8cb703f6137ebaf11c70dcd9
임현승 <lhs2689@naver.com>
2024-05-03 13:38:39 +0900

    [BE] Fix: UserCharacterInfoResponse Builder 문제 해결(@RequiredArgsConstructor 제거)

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/dto/response/UserCharacterInfoResponse.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/character/dto/response/UserCharacterInfoResponse.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 @Builder
 @Getter
-@RequiredArgsConstructor
 public class UserCharacterInfoResponse {
     private String nickname;
     private int characterId;
