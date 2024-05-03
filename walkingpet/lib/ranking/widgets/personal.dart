14cfccd916f2376408eef9c7cc628d5cbb814880
이지은 <lju0514@naver.com>
2024-05-03 17:34:57 +0900

    [FE] Fix: 앱 업로드를 위한 오류 코드 수정 및 주석처리

--- walkingpet/lib/ranking/widgets/personal.dart
+++ walkingpet/lib/ranking/widgets/personal.dart
@@ -44,105 +44,120 @@ class _PersonalRankingState extends State<PersonalRanking> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          // 1. 어제 / 실시간 / 누적 선택
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                // '어제 | 실시간 | 누적',
-                '어제자',
-                textAlign: TextAlign.right,
-              ),
-              SizedBox(
-                width: 15,
-              )
-            ],
+          // 3-1. 개인 / 그룹 / 배틀 선택
+          Text(
+            '개인랭킹',
+            style: TextStyle(fontSize: 50),
           ),
-
-          const SizedBox(
-            height: 10,
-          ),
-
-          // 2. 1~3위 표시
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ...top3.map((item) {
-                return Top1to3(
-                  ranking: item['ranking'],
-                  characterId: item['characterId'],
-                  nickname: item['nickname'],
-                  step: item['step'],
-                );
-              }),
-            ],
-          ),
-
-          // 3. 유저의 랭킹 표시
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color:
-                    const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 7),
-              child: MyRank(
-                ranking: myrank['ranking'] as int? ?? 0,
-                step: myrank['step'] as int? ?? 0,
-                characterId: myrank['characterId'] as int? ?? 0,
-                nickname: myrank['nickname'] as String? ?? 'Unknown',
-              ),
-            ),
-          ),
-
-          // 4. 4~10위 표시
-          Container(
-            // 4-1. 영역 표시
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
-              borderRadius: BorderRadius.circular(5),
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 7),
-
-            child: Column(
-              children: [
-                // 4-2. Top 10 텍스트
-                const Text(
-                  'Top 10',
-                  style: TextStyle(fontSize: 30),
-                ),
-
-                // 4-3. 내용 (스크롤 가능)
-                SizedBox(
-                  height: 230,
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ...top10.map((item) {
-                          return Rank(
-                            ranking: item['ranking'],
-                            nickname: item['nickname'],
-                            step: item['step'],
-                          );
-                        }),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          Text(
+            '- 유저확보중 -',
+            style: TextStyle(fontSize: 35),
           ),
         ],
       ),
+
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       // 1. 어제 / 실시간 / 누적 선택
+      //       const Row(
+      //         mainAxisAlignment: MainAxisAlignment.end,
+      //         children: [
+      //           Text(
+      //             // '어제 | 실시간 | 누적',
+      //             '어제자',
+      //             textAlign: TextAlign.right,
+      //           ),
+      //           SizedBox(
+      //             width: 15,
+      //           )
+      //         ],
+      //       ),
+
+      //       const SizedBox(
+      //         height: 10,
+      //       ),
+
+      //       // 2. 1~3위 표시
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           ...top3.map((item) {
+      //             return Top1to3(
+      //               ranking: item['ranking'],
+      //               characterId: item['characterId'],
+      //               nickname: item['nickname'],
+      //               step: item['step'],
+      //             );
+      //           }),
+      //         ],
+      //       ),
+
+      //       // 3. 유저의 랭킹 표시
+      //       Padding(
+      //         padding: const EdgeInsets.symmetric(vertical: 20),
+      //         child: Container(
+      //           decoration: BoxDecoration(
+      //             color:
+      //                 const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
+      //             borderRadius: BorderRadius.circular(5),
+      //           ),
+      //           margin: const EdgeInsets.symmetric(horizontal: 7),
+      //           child: MyRank(
+      //             ranking: myrank['ranking'] as int? ?? 0,
+      //             step: myrank['step'] as int? ?? 0,
+      //             characterId: myrank['characterId'] as int? ?? 0,
+      //             nickname: myrank['nickname'] as String? ?? 'Unknown',
+      //           ),
+      //         ),
+      //       ),
+
+      //       // 4. 4~10위 표시
+      //       Container(
+      //         // 4-1. 영역 표시
+      //         decoration: BoxDecoration(
+      //           color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
+      //           borderRadius: BorderRadius.circular(5),
+      //         ),
+      //         margin: const EdgeInsets.symmetric(horizontal: 7),
+
+      //         child: Column(
+      //           children: [
+      //             // 4-2. Top 10 텍스트
+      //             const Text(
+      //               'Top 10',
+      //               style: TextStyle(fontSize: 30),
+      //             ),
+
+      //             // 4-3. 내용 (스크롤 가능)
+      //             SizedBox(
+      //               height: 230,
+      //               child: SingleChildScrollView(
+      //                 scrollDirection: Axis.vertical,
+      //                 child: Column(
+      //                   mainAxisAlignment: MainAxisAlignment.center,
+      //                   children: [
+      //                     ...top10.map((item) {
+      //                       return Rank(
+      //                         ranking: item['ranking'],
+      //                         nickname: item['nickname'],
+      //                         step: item['step'],
+      //                       );
+      //                     }),
+      //                   ],
+      //                 ),
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ],
+      //   ),
     );
   }
 }
