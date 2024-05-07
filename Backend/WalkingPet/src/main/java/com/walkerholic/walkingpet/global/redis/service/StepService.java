0c39299a9b6e788b15e62dec97d5c43e6cdf29af
신유진 <yujin1698@naver.com>
2024-05-08 08:58:47 +0900

    [BE] Feat: 걸음수 저장 스프링 배치 세팅 중

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/redis/service/StepService.java
@@ -0,0 +1,39 @@
+package com.walkerholic.walkingpet.global.redis.service;
+
+import com.walkerholic.walkingpet.domain.users.entity.UserStep;
+import com.walkerholic.walkingpet.domain.users.repository.UserStepRepository;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class StepService {
+    private static final String USERS_KEY = "USER_REALTIME_STEP_RANKING_INFO";
+
+    @Resource(name = "redisTemplateForStepCount")
+    private final RedisTemplate<String, Integer> redisTemplate;
+
+    private final UserStepRepository userStepRepository;
+
+    public void updateUserStepInfo() {
+        // Get daily step for all users from Redis
+        Map<Object, Object> dailySteps = redisTemplate.opsForHash().entries(USERS_KEY);
+
+        // Process daily step for each user
+        for (Map.Entry<Object, Object> entry : dailySteps.entrySet()) {
+            String userId = (String) entry.getKey();
+            int dailyStep = (int) entry.getValue();
+
+            // Update yesterday step in MySQL and reset daily step in Redis
+            UserStep userStep = userStepRepository.findById(Integer.valueOf(userId)).orElse(null);
+            if (userStep != null) {
+                userStep.updateUserStepInfo(dailyStep);
+                userStepRepository.save(userStep);
+            }
+        }
+    }
+}
