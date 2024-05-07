291faee0b4b98321779b2a11a695684510bcea76
임현승 <lhs2689@naver.com>
2024-05-07 16:17:38 +0900

    [BE] Feat: team controller userId를 1로 고정

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/auth/filter/JwtAuthorizationFilter.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/auth/filter/JwtAuthorizationFilter.java
@@ -78,6 +78,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                         "/redis",
                         "/init",
                         "/character",
+                        "/team",
                         "/v3/api-docs",
                         "/v3/api-docs/swagger-config",
                         "/swagger-",
