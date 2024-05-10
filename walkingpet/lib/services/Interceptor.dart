1c5aff67cf94d2975034905d9ca96ba0e9e49cf7
정연우 <yoanwoo@naver.com>
2024-05-10 16:59:10 +0900

    [FE] Fix: 싱글톤 패턴을 통해 헤더에 담을 걸음수 가져오기

--- walkingpet/lib/services/Interceptor.dart
+++ walkingpet/lib/services/Interceptor.dart
@@ -1,14 +1,25 @@
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:walkingpet/providers/step_counter.dart';
 
 class AuthInterceptor extends http.BaseClient {
   final http.Client _inner = http.Client();
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  final StepCounter _stepCounter; // StepCounter 인스턴스 추가
+
+  static final AuthInterceptor _instance = AuthInterceptor._internal();
+
+  factory AuthInterceptor() {
+    return _instance;
+  }
+
+  AuthInterceptor._internal() : _stepCounter = StepCounter();
+  // 싱글톤 인스턴스
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
     final accessToken = await _storage.read(key: 'ACCESS_TOKEN');
-    int steps = 1000;
+    final steps = _stepCounter.steps ?? 0; // StepCounter의 steps 값 사용
     final headers = request.headers;
 
     headers['Authorization'] = 'Bearer $accessToken';
