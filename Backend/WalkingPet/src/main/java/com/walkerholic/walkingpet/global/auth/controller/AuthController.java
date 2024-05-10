6e3987916d9d6f5de1bf1c68a33bae3e1026f399
임현승 <lhs2689@naver.com>
2024-05-10 13:10:22 +0900

    [BE] Feat: 더미데이터 설정하는 파일 추가

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/auth/controller/AuthController.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/auth/controller/AuthController.java
@@ -23,8 +23,6 @@ public class AuthController {
     private final SecurityService securityService;
     private final JwtUtil jwtUtil;
 
-
-
     @PostMapping("/social-login")
     public ResponseEntity<CommonResponseEntity> socialLogin(@RequestBody SocialLoginDTO socialLoginDTO) //TODO: @Valid 추가
     {
@@ -34,31 +32,5 @@ public class AuthController {
         securityService.saveUserInSecurityContext(savedOrFindUser);
         Map<String, String> tokenMap = jwtUtil.initToken(savedOrFindUser);
         return CommonResponseEntity.toResponseEntity(GlobalSuccessCode.SUCCESS, tokenMap);
-    }
-
-    @GetMapping("/generate/user")
-    public void generateUsers()
-    {
-        String[] gameNicknames = {
-                "하늘", "바다", "나무", "바람", "구름", "태양", "이슬", "노을", "꽃잎", "새싹",
-                "나비", "벌꿀", "강아지", "고양이", "토끼", "곰돌이", "병아리", "다람쥐", "기린", "호랑이",
-                "사자", "코끼리", "원숭이", "펭귄", "물개", "고래", "거북이", "개구리", "뱀", "악어",
-                "돌고래", "가재", "새우", "소라", "조개", "불가사리", "참치", "연어", "고등어", "갈치",
-                "멸치", "오징어", "가오리", "해마", "해파리", "미역", "김", "다시마", "사과", "배",
-                "바나나", "오렌지", "망고", "수박", "참외", "메론", "감자", "옥수수", "당근", "양파",
-                "배추", "상추", "깻잎", "고추", "마늘", "펭귄", "고래", "가오리", "해마", "배",
-                "포도", "바나나", "망고", "수박", "참외", "감자", "고구마", "옥수수", "당근",
-                "양파", "배추", "상추", "깻잎", "고추", "마늘", "펭귄", "고래", "가오리", "해마",
-                "딸기", "바나나", "망고", "수박", "참외", "감자", "옥수수", "당근",
-                "양파", "배추", "상추", "깻잎", "고추", "마늘"
-        };
-
-        for(int i = 0; i < gameNicknames.length ; i++){
-            String testEmail = "dummy test2" + i + "@gmail.com";
-            String testNickname = gameNicknames[i] + "중독";
-            SocialLoginDTO socialLoginDTO = new SocialLoginDTO(testEmail,testNickname);
-            UsersDto savedOrFindUser = loginService.socialLogin(socialLoginDTO);
-            securityService.saveUserInSecurityContext(savedOrFindUser);
-        }
     }
 }
