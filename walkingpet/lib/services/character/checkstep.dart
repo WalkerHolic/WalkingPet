9b4e0876926588f91e7a0f3c3f76b13f73e7445f
여진구 <dmdkvj369@naver.com>
2024-05-12 00:00:47 +0900

    [FE] Fix: 걸음수 fetch uri 변경

--- walkingpet/lib/services/character/checkstep.dart
+++ walkingpet/lib/services/character/checkstep.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';        // JSON 데이터를 다루고, 문자열 인코딩 및 디코딩을 위함
+import 'dart:convert'; // JSON 데이터를 다루고, 문자열 인코딩 및 디코딩을 위함
 import '../Interceptor.dart';
 
 // Future<Void>는 반환 값이 없고, 완료까지 일정 시간이 소요될 수 있는 비동기 작업을 수행
@@ -8,8 +8,8 @@ Future<int> checkStep() async {
   await 키워드는 비동기 요청의 완료를 기다리며, 그 동안 다른 코드의 실행을 중지하지 않습니다.
   */
   final client = AuthInterceptor();
-  final response =
-      await client.get(Uri.parse('https://walkingpet.co.kr/character/checkstep'));
+  final response = await client
+      .get(Uri.parse('https://walkingpet.co.kr/user/checkstep?userId=1'));
 
   if (response.statusCode == 200) {
     // 응답 바이트를 UTF-8로 디코딩 (UTF8 디코딩 안하면 한글이 깨져서 온다)
