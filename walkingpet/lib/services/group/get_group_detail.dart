86cfecbf1b47e090ce58b568faae93f0cd3ff8ef
Seyoung Jang <joy18860@gmail.com>
2024-05-13 15:25:19 +0900

    [FE] Feat: 그룹 만들기 버튼 생성, 그룹 생성 페이지 라우터 연결

--- walkingpet/lib/services/group/get_group_detail.dart
+++ walkingpet/lib/services/group/get_group_detail.dart
@@ -1,8 +1,10 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../Interceptor.dart';
 
 Future<Map<String, dynamic>> getGroupDetail(groupId) async {
-  final response = await http
+  final client = AuthInterceptor();
+  final response = await client
       .get(Uri.parse('https://walkingpet.co.kr/team/detail/$groupId'));
   if (response.statusCode == 200) {
     var data = utf8.decode(response.bodyBytes);
--- walkingpet/lib/services/group/get_my_group.dart
+++ walkingpet/lib/services/group/get_my_group.dart
@@ -3,8 +3,9 @@ import 'dart:convert';
 import '../Interceptor.dart'; // JSON 데이터를 다루고, 문자열 인코딩 및 디코딩을 위함
 
 Future<List<Map<String, dynamic>>> getMyGroup() async {
+  final client = AuthInterceptor();
   final response =
-      await http.get(Uri.parse('https://walkingpet.co.kr/team/belong'));
+      await client.get(Uri.parse('https://walkingpet.co.kr/team/belong'));
   if (response.statusCode == 200) {
     //  응답 바이트를 UTF-8로 디코딩 (디코팅 안하면 한글이 깨져서 온다)
     var data = utf8.decode(response.bodyBytes);
@@ -16,7 +17,7 @@ Future<List<Map<String, dynamic>>> getMyGroup() async {
     }).toList();
   } else {
     //상태 코드와 함께 에러 메세지 출력력
-    print("에러남 : ${response.statusCode}");
+    print("내가 가입한 그룹 데이터 받아오기 오류 : ${response.statusCode}");
     throw Error();
   }
 }
