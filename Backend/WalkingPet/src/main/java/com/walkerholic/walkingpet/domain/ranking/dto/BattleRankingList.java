7e6912c57a6d108944d96306dfe589a2e23b7241
신유진 <yujin1698@naver.com>
2024-05-13 12:56:50 +0900

    [BE] Feat: 배틀 랭킹 캐싱 적용

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/ranking/dto/BattleRankingList.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/ranking/dto/BattleRankingList.java
@@ -2,18 +2,25 @@ package com.walkerholic.walkingpet.domain.ranking.dto;
 
 import com.walkerholic.walkingpet.domain.users.dto.UserRedisDto;
 import com.walkerholic.walkingpet.domain.users.entity.UserDetail;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BattleRankingList {
     private int userId;
     private String nickname;
     private int characterId;
     private int ranking;
     private int battleRating;
+
+    @Builder
+    public BattleRankingList(int userId, String nickname, int characterId, int ranking, int battleRating) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.characterId = characterId;
+        this.ranking = ranking;
+        this.battleRating = battleRating;
+    }
 
     public static BattleRankingList from(UserDetail userDetail, int ranking) {
         return BattleRankingList.builder()
--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/ranking/dto/response/BattleRankingResponse.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/ranking/dto/response/BattleRankingResponse.java
@@ -1,16 +1,23 @@
 package com.walkerholic.walkingpet.domain.ranking.dto.response;
 
 import com.walkerholic.walkingpet.domain.ranking.dto.BattleRankingList;
-import com.walkerholic.walkingpet.domain.ranking.dto.PersonalStepRankingInfo;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+// redis 캐싱에도 사용
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BattleRankingResponse {
     private List<BattleRankingList> topRanking;
+
+    @Builder
+    public BattleRankingResponse(List<BattleRankingList> topRanking) {
+        this.topRanking = topRanking;
+    }
 
     public static BattleRankingResponse from(List<BattleRankingList> accStepRanking) {
         return BattleRankingResponse.builder()
