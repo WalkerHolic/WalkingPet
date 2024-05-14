1cf62a93f275ad36ea232d754d0839d78c9ccb63
Njini1 <dbwls6627@gmail.com>
2024-05-15 02:59:09 +0900

    [BE] Chore: redis yml 파일 수정

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/config/RedisConfig.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -20,10 +21,18 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     // RedisConnectionFactory 빈을 생성하여 Redis에 연결
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host, port);
+
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisConfiguration.setPassword(password);
+
+        return new LettuceConnectionFactory(redisConfiguration);
+//        return new LettuceConnectionFactory(host, port);
     }
 
     // 데이터 저장 가능
