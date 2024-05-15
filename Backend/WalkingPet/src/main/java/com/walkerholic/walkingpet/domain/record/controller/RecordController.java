96fb70348ecf6488bf9fa66c2c2456a7427ce32a
임현승 <lhs2689@naver.com>
2024-05-15 18:39:16 +0900

    [BE] Feat: Record Controller 추가

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/controller/RecordController.java
@@ -0,0 +1,18 @@
+package com.walkerholic.walkingpet.domain.record.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/record")
+public class RecordController {
+
+    //1. 사진 올리기
+
+    //2. 사진과 텍스트 캐릭터 아이디 가져오기(userId 와 recordId로 가져오기)
+
+    //3. 유저가 올린 모든 사진 가져오기(유저 id로 저장되어 있는 모든 사진 가져오기)
+}
