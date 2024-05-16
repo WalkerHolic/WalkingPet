edfa60ac4e987623a3137cb4bd5488023602c611
임현승 <lhs2689@naver.com>
2024-05-16 17:09:01 +0900

    [BE] Feat: 유저 아이디와 이미지 이름으로 기록 지우는 코드 추가

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/repository/RecordRepository.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/repository/RecordRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecordRepository extends JpaRepository<Record, Integer> {
     @Query("SELECT R FROM Record R WHERE R.user.userId = :userId")
     List<Record> findAllByUserId(int userId);
+
+    @Query("SELECT R FROM Record R WHERE R.user.userId = :userId AND R.imageName = :imageName")
+    Optional<Record> findByUserIdAndImageName(int userId, String imageName);
 }
