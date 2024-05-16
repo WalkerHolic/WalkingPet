bc9db2af0395646c28a7f24e7d6e67f7616e8aef
이지은 <lju0514@naver.com>
2024-05-16 14:32:31 +0900

    [FE] Design: 랭킹 페이지 전체 UI 수정 및 그룹랭킹 주석 해제

--- walkingpet/lib/ranking/widgets/top3.dart
+++ walkingpet/lib/ranking/widgets/top3.dart
@@ -14,6 +14,10 @@ class Top1to3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 현재 화면의 크기 가져오기
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     String animal = CharacterMap.idToAnimal[characterId] ?? "Unknown";
 
     return Padding(
@@ -25,26 +29,27 @@ class Top1to3 extends StatelessWidget {
           // 1. 순위 (1~3위)
           // 1,2,3위는 해당하는 메달 이미지를, 나머지는 텍스트만 나오도록 수정
           SizedBox(
-            width: 36,
+            // width: 36,
+            width: screenWidth * 0.1,
             child: ranking == 1
                 ? Image.asset(
                     'assets/icons/gold_medal.png',
-                    width: 36,
-                    height: 36,
+                    width: screenWidth * 0.1,
+                    height: screenHeight * 0.05,
                     fit: BoxFit.contain,
                   )
                 : ranking == 2
                     ? Image.asset(
                         'assets/icons/silver_medal.png',
-                        width: 36,
-                        height: 36,
+                        width: screenWidth * 0.1,
+                        height: screenHeight * 0.05,
                         fit: BoxFit.contain,
                       )
                     : ranking == 3
                         ? Image.asset(
                             'assets/icons/bronze_medal.png',
-                            width: 36,
-                            height: 36,
+                            width: screenWidth * 0.1,
+                            height: screenHeight * 0.05,
                             fit: BoxFit.contain,
                           )
                         : Text(
@@ -59,8 +64,10 @@ class Top1to3 extends StatelessWidget {
           // 2. 캐릭터 이미지
           Image.asset(
             'assets/animals/$animal/${animal}_idle.gif',
-            height: 90,
-            width: 90,
+            // height: 90,
+            // width: 90,
+            height: screenHeight * 0.11,
+            width: screenWidth * 0.23,
           ),
 
           // 걸음수 생략
