2d2e05488754718fc3d9b38d9a4d87854d55f103
이지은 <lju0514@naver.com>
2024-05-16 10:30:22 +0900

    [FE] 기록 페이지 퍼블리싱

---
+++ walkingpet/lib/record/widgets/map.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:kakaomap_webview/kakaomap_webview.dart';
+
+class RecordMap extends StatelessWidget {
+  const RecordMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('data');
+  }
+}
