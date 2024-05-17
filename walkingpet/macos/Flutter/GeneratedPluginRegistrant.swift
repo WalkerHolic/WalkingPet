f2917fe1e2f21ef92c2985ed0c956a7b3666c642
여진구 <dmdkvj369@naver.com>
2024-05-18 01:20:03 +0900

    [FE] Design: 캐릭터 변경 모달 깨지는 문제 수정

--- walkingpet/macos/Flutter/GeneratedPluginRegistrant.swift
+++ walkingpet/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_core
 import firebase_messaging
 import flutter_blue_plus
 import flutter_local_notifications
+import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
