743c7cf4b378b7fcdd4c25acf6392f47fe98e07c
임현승 <lhs2689@naver.com>
2024-05-17 14:16:53 +0900

    [BE] Feat: 이벤트 기록 반환 Response 코드 추가

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/dto/response/EventRecordResponse.java
@@ -0,0 +1,17 @@
+package com.walkerholic.walkingpet.domain.record.dto.response;
+
+import com.walkerholic.walkingpet.domain.record.dto.EventRecord;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Slf4j
+@Getter
+public class EventRecordResponse {
+    @Builder.Default
+    List<EventRecord> enventRecordList = new ArrayList<>();
+}
