37f581391c8ac80f7428d8e9e16769fcba1a53c9
여진구 <dmdkvj369@naver.com>
2024-05-18 13:11:25 +0900

    [FE] Fix: 목표페이지 SnackBar 쌓이는 문제 수정

--- walkingpet/lib/goal/widgets/daily.dart
+++ walkingpet/lib/goal/widgets/daily.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:walkingpet/main.dart';
 import 'package:walkingpet/services/goal/get_daily_reward.dart';
 
 class DailyGoalItem extends StatefulWidget {
   final String title;
-  // 서버에서 받은 데이터에 따라 초기 활성화 상태 변경
-  final bool isActivated; //활성화 여부
+  final bool isActivated;
   final bool isCompleted;
-  final int goalSteps; // 각각 목표에 따라 리워드 지급 요청을 보내기 위해
+  final int goalSteps;
 
   const DailyGoalItem({
-    //생성자
     super.key,
     required this.title,
-    //기존 상태 = 비활성
     required this.isActivated,
     required this.isCompleted,
     required this.goalSteps,
   });
+
   @override
   _DailyGoalItemState createState() => _DailyGoalItemState();
 }
@@ -29,53 +26,51 @@ class _DailyGoalItemState extends State<DailyGoalItem> {
   @override
   void initState() {
     super.initState();
-    //목표가 이미 완료된 경우 _isPressed를 true로 설정
     _isPressed = widget.isCompleted;
   }
 
   void _toggleButton() async {
-    // 버튼이 이미 완료된 상태라면 요청을 보내지 않음
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    // 현재 표시된 스낵바가 있다면 숨김
+    scaffoldMessenger.hideCurrentSnackBar();
+
     if (_isPressed || widget.isCompleted) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("이미 완료된 목표입니다."),
-        duration: Duration(seconds: 2),
-      ));
-      return; // 더 이상의 작업을 수행하지 않고 함수 종료
+      _showSnackBar("이미 완료된 목표입니다.");
+      return;
     }
 
-    // 버튼이 비활성화된 경우
     if (!widget.isActivated) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("이 목표는 활성화되지 않았습니다."),
-        duration: Duration(seconds: 2),
-      ));
-      return; // 비활성화 상태에서 요청을 보내지 않고 함수 종료
+      _showSnackBar("이 목표는 활성화되지 않았습니다.");
+      return;
     }
 
-    // 보상 요청을 서버로 보냄
     sendRewardRequest(widget.goalSteps).then((reward) {
       setState(() {
         _isPressed = true;
       });
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text("보상이 지급되었습니다."),
-          // content: Text("보상이 지급되었습니다. : $reward"),
-          duration: Duration(seconds: 2),
-        ),
-      );
+      _showSnackBar("보상이 지급되었습니다.");
     });
+  }
+
+  void _showSnackBar(String message) {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    // 현재 표시된 스낵바가 있다면 숨김
+    scaffoldMessenger.hideCurrentSnackBar();
+
+    scaffoldMessenger.showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 1),
+      ),
+    );
   }
 
   String getButtonAsset() {
     if (_isPressed) {
-      print('이미 달성된 경우 : $_isPressed');
-      //이미 달성된 경우
       return 'assets/buttons/button_complete.svg';
     } else if (widget.isActivated) {
-      //달성되지 않았지만 활성화된 경우
-      print('달성되지 않았지만 활성화된 경우 : $_isPressed');
-      //왜 isPressed가 false로 뜨지?
       return 'assets/buttons/brown_button.svg';
     } else {
       return 'assets/buttons/button_not_enough.svg';
