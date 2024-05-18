67f2e0aab3f8225a2651ebf8eb7994ec41a040ad
임현승 <lhs2689@naver.com>
2024-05-18 15:03:04 +0900

    [BE] Feat: 일반 기록 조회 반환 ResponseDto 요소 파일 추가

---
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/dto/NormalRecord.java
@@ -0,0 +1,30 @@
+package com.walkerholic.walkingpet.domain.record.dto;
+
+import com.walkerholic.walkingpet.domain.record.entity.Record;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
+
+@Getter
+@Slf4j
+@Builder
+public class NormalRecord {
+    private String imageUrl;
+    private int recordId;
+    private int characterId;
+    private double latitude;
+    private double longitude;
+    private LocalDateTime regDate;
+
+    public static NormalRecord from(Record record){
+        return NormalRecord.builder()
+                .imageUrl(record.getImageUrl())
+                .recordId(record.getRecordId())
+                .characterId(record.getCharacterId())
+                .latitude(record.getLatitude().doubleValue())
+                .longitude(record.getLongitude().doubleValue())
+                .regDate(record.getRecordRegDate())
+                .build();
+    }
+}
