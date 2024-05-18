9aa462a09d4a9a055d17b53af285e151a93b3d35
신유진 <yujin1698@naver.com>
2024-05-18 16:41:59 +0900

    [BE] Feat: redis에 유저 배틀 점수 저장

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/redis/service/BattleRankingRedisService.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/global/redis/service/BattleRankingRedisService.java
@@ -1,10 +1,17 @@
 package com.walkerholic.walkingpet.global.redis.service;
 
-import com.walkerholic.walkingpet.domain.ranking.dto.request.RealtimeStepRequest;
+import com.walkerholic.walkingpet.domain.ranking.dto.BattleRankingList;
+import com.walkerholic.walkingpet.domain.ranking.dto.response.BattleRankingResponse;
+import com.walkerholic.walkingpet.domain.users.dto.UserRedisDto;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +21,12 @@ public class BattleRankingRedisService {
     @Resource(name = "redisTemplateForStepCount")
     private final RedisTemplate<String, Integer> rankigRedisTemplate;
 
-//    public void saveUserDailyStep(RealtimeStepRequest realtimeStepRequest) {
-//        rankigRedisTemplate.opsForZSet().add(STEP_KEY, realtimeStepRequest.getUserId(), realtimeStepRequest.getRealtimeStep());
-//    }
+    private final UserInfoRedisService userInfoRedisService;
+
+    // redis에 userId와 battle 점수 저장
+    @Transactional(readOnly = false)
+    public void saveUserBattleScore(int userId, int battleRanking) {
+        rankigRedisTemplate.opsForZSet().add(RANKING_KEY, userId, battleRanking);
+    }
+
 }
