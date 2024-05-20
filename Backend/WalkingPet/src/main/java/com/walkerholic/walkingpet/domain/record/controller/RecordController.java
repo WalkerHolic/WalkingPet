f973e72ff74d8e11c699a5fbbf96c760f3f57a8d
임현승 <lhs2689@naver.com>
2024-05-20 09:58:55 +0900

    [BE] Fix: userId 고정된 값 수정

--- Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/controller/RecordController.java
+++ Backend/WalkingPet/src/main/java/com/walkerholic/walkingpet/domain/record/controller/RecordController.java
@@ -34,8 +34,7 @@ public class RecordController {
                                                              @RequestParam("characterId") int characterId,
                                                              @RequestParam("latitude") double latitude,
                                                              @RequestParam("longitude") double longitude) {
-//        int userId = userDetail.getUsers().getUserId();
-        int userId = 25;
+        int userId = userDetail.getUsers().getUserId();
         log.info("유저의 기록 등록하기 - userId: {}", userId);
 
         UploadRecordResponse uploadRecordResponse = recordService.uploadRecord(userId, multipartFile, characterId, latitude, longitude);
@@ -48,8 +47,7 @@ public class RecordController {
     @ApiResponse(responseCode = "200", description = "S200 - 가입한 회원인지 확인 성공", content = @Content(schema = @Schema(implementation = CommonResponseEntity.class)))
     @ApiResponse(responseCode = "404", description = "R400 - 유저가 작성한 기록 없음")
     public ResponseEntity<CommonResponseEntity> getAllRecord(@AuthenticationPrincipal CustomUserDetail userDetail) {
-//        int userId = userDetail.getUsers().getUserId();
-        int userId = 47;
+        int userId = userDetail.getUsers().getUserId();
         log.info("유저가 등록한 모든 기록 가져오기 - userId: {}", userId);
         AllUserRecordResponse allUserRecordResponse = recordService.getAllRecord(userId);
 
@@ -110,8 +108,7 @@ public class RecordController {
     @ApiResponse(responseCode = "200", description = "S200 - 기록 조회 성공", content = @Content(schema = @Schema(implementation = CommonResponseEntity.class)))
     @ApiResponse(responseCode = "404", description = "R400 - 기록 조회 실패")
     public ResponseEntity<CommonResponseEntity> loadEventRecord(@AuthenticationPrincipal CustomUserDetail userDetail){
-//        int userId = userDetail.getUsers().getUserId();
-        int userId = 47;
+        int userId = userDetail.getUsers().getUserId();
         log.info("선택한 기록이 가까운지 멀리 있는지 확인 - userId: {}", userId);
         EventRecordResponse eventRecordResponse = recordService.loadEventRecord(userId);
 
@@ -144,8 +141,7 @@ public class RecordController {
                                                              @RequestParam("latitude") double latitude,
                                                              @RequestParam("longitude") double longitude,
                                                              @RequestParam("content") String content) {
-//        int userId = userDetail.getUsers().getUserId();
-        int userId = 32;
+        int userId = userDetail.getUsers().getUserId();
         log.info("이벤트 기록 등록하기 - userId: {}", userId);
 
         UploadRecordResponse uploadRecordResponse = recordService.uploadEventRecord(userId, multipartFile, characterId, latitude, longitude, content);
